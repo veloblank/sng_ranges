94f6d7108cb3bd5f8a2b25fbb9a9c72c4cac0e57
John Blank <veloblank@gmail.com>
2020-09-26 20:26:42 -0400

    Create hidden class that toggles on btn.

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 window.onload = addListeners();
 
 function addListeners() {
+  let showPushRange = document.getElementById("push-range-btn");
+  let showCallRange = document.getElementById("call-range-btn");
+  showPushRange.addEventListener("click", togglePushRange)
+  showCallRange.addEventListener("click", toggleCallRange)
 }
 
+function toggleCallRange() {
+  let callTable = document.getElementById("bloch_call_hidden");
+  callTable.classList.toggle("hidden")
+  console.log(callTable.classList)
+}
+
+function togglePushRange() {
+  let pushTable = document.getElementById("bloch_jam_hidden");
+  pushTable.classList.toggle("hidden")
+  console.log(pushTable.classList)
+}
 
